5e1ae4bdb567c8c6d588bc3080a0584bdce7262b
Kamil Pajdzik <pajdziupaj@gmail.com>
2017-06-28 19:52:11 -0700

    Add solution draft

---
+++ src/EzPassword.WordDownloader/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EzPassword.WordDownloader
+{
+    using System.Threading.Tasks;
+    using WikiClientLibrary;
+    using WikiClientLibrary.Client;
+    using WikiClientLibrary.Generators;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var wikiClient = new WikiClient();
+            var site = await Site.CreateAsync(wikiClient, "https://pl.wiktionary.org/w/api.php");
+            var categoryMembersGenerator = new CategoryMembersGenerator(site, "Kategoria:Język polski - przymiotniki")
+            {
+                MemberTypes = CategoryMemberTypes. Page
+            };
+
+            var wikiDownloader = new WikiDownloader(categoryMembersGenerator);
+            wikiDownloader.DownloadWords("C:\\Temp\\dicts", "adjectives_{0:D2}.txt");
+        }
+    }
+}
---
+++ src/EzPassword.WordDownloader/WikiDownloader.cs
@@ -0,0 +1,46 @@
+﻿namespace EzPassword.WordDownloader
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using WikiClientLibrary.Generators;
+
+    internal class WikiDownloader
+    {
+        private readonly CategoryMembersGenerator categoryMembersGenerator;
+        private readonly IDictionary<int, StreamWriter> streamWriters;
+
+        public WikiDownloader(CategoryMembersGenerator categoryMembersGenerator)
+        {
+            this.categoryMembersGenerator = categoryMembersGenerator;
+            this.streamWriters = new Dictionary<int, StreamWriter>();
+        }
+
+        public void DownloadWords(string directoryPath, string fileNameTemplate)
+        {
+            Directory.CreateDirectory(directoryPath);
+
+            this.categoryMembersGenerator.EnumPagesAsync().Take(500).ForEach((page, i) =>
+            {
+                var streamWriter = this.GetStreamWriter(page.Title.Length, directoryPath, fileNameTemplate);
+                streamWriter.WriteLineAsync(page.Title).Wait();
+            });
+
+            foreach (var streamWriter in this.streamWriters.Values)
+            {
+                streamWriter.Dispose();
+            }
+        }
+
+        private StreamWriter GetStreamWriter(int wordLength, string directoryPath, string fileNameTemplate)
+        {
+            if (!this.streamWriters.ContainsKey(wordLength))
+            {
+                var fileName = string.Format($"{directoryPath}/{fileNameTemplate}", wordLength);
+                this.streamWriters[wordLength] = File.AppendText(fileName);
+            }
+
+            return this.streamWriters[wordLength];
+        }
+    }
+}
