adc4e0f2334ef0b2a27f18b64a00c2656b06e913
Kamil Pajdzik <pajdziupaj@gmail.comn>
2017-07-26 22:07:16 -0700

    Add PasswordGeneratingChain

---
+++ src/EzPassword.Core/PasswordGeneratingChain.cs
@@ -0,0 +1,12 @@
+﻿namespace EzPassword.Core
+{
+    public class PasswordGeneratingChain
+    {
+        private IRandomWordGenerator[] randomWordGenerators;
+
+        public PasswordGeneratingChain(params IRandomWordGenerator[] randomWordGenerators)
+        {
+            this.randomWordGenerators = randomWordGenerators;
+        }
+    }
+}
