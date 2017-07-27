a22973ac7c03123f5f15488347212dae209b05e0
Kamil Pajdzik <pajdziupaj@gmail.comn>
2017-07-26 22:09:13 -0700

    Add Password and PasswordType

---
+++ src/EzPassword.Core/PasswordType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzPassword.Core
+{
+    [Flags]
+    public enum PasswordType
+    {
+        Unknown = 0,
+        Words = 1 << 1,
+        WordsSeparatedBySpecialCharacters = 1 << 2,
+        LeftPadedBySpecialCharactes
+    }
+}
