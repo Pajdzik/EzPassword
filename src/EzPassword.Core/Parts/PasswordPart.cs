3a3396710ef8ac1a7033ff13e7dcb934debd23ac
Kamil Pajdzik <pajdziu@live.com>
2020-01-08 21:12:11 -0800

    Add new password parts

--- src/EzPassword.Core/Parts/PasswordPart.cs
+++ src/EzPassword.Core/Parts/PasswordPart.cs
@@ -1,6 +1,7 @@
 ﻿namespace EzPassword.Core.Parts
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public abstract class PasswordPart
     {
@@ -9,8 +10,18 @@
             this.Content = content;
         }
 
+        public PasswordPart(string content)
+            : this(content.ToCharArray())
+        {
+        }
+
         public IList<char> Content { get; private set; }
 
         public int Length => this.Content.Count;
+
+        public override string ToString()
+        {
+            return new string(this.Content.ToArray());
+        }
     }
 }
