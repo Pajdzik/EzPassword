79f7da28a8c2baafb862e116d03aca041e384b27
Kamil Pajdzik <pajdziu@live.com>
2020-04-12 22:19:21 -0700

    Add ChangeCaseTransformationTests

---
+++ src/EzPassword.Transformation.UnitTests/ChangeCaseTransformationTests.cs
@@ -0,0 +1,23 @@
+namespace EzPassword.Transformation.UnitTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using EzPassword.Core;
+    using EzPassword.Core.Parts;
+    using EzPassword.Transformation;
+    using FluentAssertions;
+    using Xunit;
+
+    public class UpperCaseTransformationTests
+    {
+        public class Transform
+        {
+            [Fact]
+            public void Foo()
+            {
+                var upperCase = new UpperCaseTransformation();
+                upperCase.Transform()
+            }
+        }
+    }
+}
