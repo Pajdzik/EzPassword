9801904d7b616e2e59c03934ccb99a74a684bdac
Kamil Pajdzik <pajdziupaj@gmail.com>
2020-12-12 19:17:39 -0800

    Add draft Azure Function

---
+++ src/EzPassword.Function/Startup.cs
@@ -0,0 +1,31 @@
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(EzPassword.Function.Startup))]
+namespace EzPassword.Function
+{
+    using System.IO;
+    using System.Reflection;
+    using EzPassword.Core;
+    using EzPassword.Core.Config;
+    using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string? dllPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string wordsDirectory = Path.Combine(dllPath ?? "", "content", "pl");
+
+            var passwordGenerator = PasswordGeneratorFactory.Create(
+                    Path.Join(wordsDirectory, "adjectives"),
+                    Path.Join(wordsDirectory, "nouns"),
+                    WordDirectoryConfig.AdjectiveFileNameRegex,
+                    WordDirectoryConfig.NounFileNameRegex
+                );
+            var serviceDescriptor = new ServiceDescriptor(typeof(PasswordGenerator), passwordGenerator);
+            builder.Services.Add(serviceDescriptor);
+        }
+    }
+}
