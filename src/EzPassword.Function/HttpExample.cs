c78094c7b137ef713e697a317ee23bef6e964b63
Kamil Pajdzik <pajdziupaj@gmail.com>
2021-06-16 21:27:10 -0700

    Add Azure Function (#19)
    * Add draft Azure Function
    
    * Fix source words copy
    
    * New changes
    
    * Remove old Function
    
    * Add Function v4
    
    * Configure csproj
    
    * Finish configuration
    
    * Add simple implementation of HTTP trigger
    
    * Add local settings
    
    * Add .NET core 3.1 SDK

---
+++ src/EzPassword.Function/HttpExample.cs
@@ -0,0 +1,36 @@
+namespace EzPassword.Function
+{
+    using System.Net;
+    using EzPassword.Core;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Azure.Functions.Worker.Http;
+    using Microsoft.Extensions.Logging;
+    
+    public class HttpExample
+    {
+        private readonly PasswordGenerator passwordGenerator;
+
+        public HttpExample(PasswordGenerator passwordGenerator)
+        {
+            this.passwordGenerator = passwordGenerator;
+        }
+
+        [Function("EzPassword")]
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
+            HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("HttpExample");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            Password password = passwordGenerator.Generate(20);
+            response.WriteString(password.ToString());
+
+            return response;
+        }
+    }
+}
