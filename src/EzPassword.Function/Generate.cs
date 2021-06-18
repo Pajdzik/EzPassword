3834578d20e41a029550938c46d3fef133bb1357
Kamil Pajdzik <pajdziupaj@gmail.com>
2021-06-17 23:16:11 -0700

    Add parameter handling to Azure Function

---
+++ src/EzPassword.Function/Generate.cs
@@ -0,0 +1,100 @@
+namespace EzPassword.Function
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using EzPassword.Core;
+    using EzPassword.Transformation;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Azure.Functions.Worker.Http;
+    using Microsoft.Extensions.Logging;
+
+    public class Generate
+    {
+        private readonly PasswordGenerator passwordGenerator;
+
+        public Generate(PasswordGenerator passwordGenerator)
+        {
+            this.passwordGenerator = passwordGenerator;
+        }
+
+        [Function("Generate")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
+            HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger(nameof(Generate));
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            var parameters = await this.ParseParameters(req);
+            string output = this.GetOutput(parameters);
+            response.WriteString(output);
+
+            return response;
+        }
+
+        private async Task<RestPasswordParameters> ParseParameters(HttpRequestData request)
+        {
+            string requestBody = string.Empty;
+            using (StreamReader streamReader = new StreamReader(request.Body))
+            {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var parameters = JsonSerializer.Deserialize<RestPasswordParameters>(requestBody, options);
+
+            return parameters;
+        }
+
+        private string GetOutput(RestPasswordParameters parameters)
+        {
+            var passwords = new List<Password>();
+            Transformer transformer = TransformerFactory.CreateFromKeywords(parameters.Transformations);
+
+            for (int i = 0; i < parameters.PasswordCount; i++)
+            {
+                Password password = passwordGenerator.Generate(parameters.PasswordLength);
+                Password transformedPassword = transformer.Transform(password);
+                passwords.Add(transformedPassword);
+            }
+
+            return this.GenerateOutput(passwords, parameters.JsonOutput);
+        }
+
+        private string GenerateOutput(IEnumerable<Password> passwords, bool generateJson)
+        {
+            IEnumerable<string> verbatimPasswords = passwords.Select(p => p.ToString());
+            return generateJson
+                    ? this.GenerateJsonOutput(verbatimPasswords)
+                    : this.GenerateTextOutput(verbatimPasswords);
+        }
+
+        private string GenerateJsonOutput(IEnumerable<string> passwords)
+        {
+            if (passwords.Count() == 1)
+            {
+                return JsonSerializer.Serialize(passwords.First());
+            }
+
+            return JsonSerializer.Serialize(passwords);
+        }
+
+        private string GenerateTextOutput(IEnumerable<string> passwords)
+        {
+            return string.Join(Environment.NewLine, passwords);
+        }
+    }
+}
